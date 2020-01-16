c476479c327c585a319068aa793cf26e0a211be7
Adrian Santo Pasqualini <aspasqualini@uwaterloo.ca>
2020-01-16 18:45:41 -0500

    Updates

---
+++ precache-manifest.64a978506302ddb447911e42286d6b6b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e00f593027efd74a3ec0d36956f0d8a8",
+    "url": "/PocketParliamentWebApp/index.html"
+  },
+  {
+    "revision": "2d91a68a26c04e5c8d15",
+    "url": "/PocketParliamentWebApp/static/css/2.0b7c9a72.chunk.css"
+  },
+  {
+    "revision": "5b3270c5cdacc504f426",
+    "url": "/PocketParliamentWebApp/static/css/main.3e32f755.chunk.css"
+  },
+  {
+    "revision": "2d91a68a26c04e5c8d15",
+    "url": "/PocketParliamentWebApp/static/js/2.73fd23be.chunk.js"
+  },
+  {
+    "revision": "714c147d840f36980f507a00da3599d3",
+    "url": "/PocketParliamentWebApp/static/js/2.73fd23be.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5b3270c5cdacc504f426",
+    "url": "/PocketParliamentWebApp/static/js/main.dc2ecd63.chunk.js"
+  },
+  {
+    "revision": "7269b0c4207b44d8a9e4f0b85c8a81d2",
+    "url": "/PocketParliamentWebApp/static/js/main.dc2ecd63.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dd717d2447404b7e3bce",
+    "url": "/PocketParliamentWebApp/static/js/runtime-main.4a693acc.js"
+  },
+  {
+    "revision": "600695d09aba3a355a06f9bae586174c",
+    "url": "/PocketParliamentWebApp/static/media/MissingPhotoAvatar.600695d0.jpg"
+  },
+  {
+    "revision": "689e7d30d0112a3b28f0da450ff1a859",
+    "url": "/PocketParliamentWebApp/static/media/parliamentvertical.689e7d30.jpg"
+  }
+]);
