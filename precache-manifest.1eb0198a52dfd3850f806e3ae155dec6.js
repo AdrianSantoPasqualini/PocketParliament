bb0bbd2930613211eafeb596de0974ab5d6a597a
Adrian Santo Pasqualini <aspasqualini@uwaterloo.ca>
2020-01-17 22:27:46 -0500

    Updates

---
+++ precache-manifest.1eb0198a52dfd3850f806e3ae155dec6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bcea4649055391291a5641bb3b6c63f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d91a68a26c04e5c8d15",
+    "url": "/static/css/2.0b7c9a72.chunk.css"
+  },
+  {
+    "revision": "d3c353a5179f743018c7",
+    "url": "/static/css/main.3e32f755.chunk.css"
+  },
+  {
+    "revision": "2d91a68a26c04e5c8d15",
+    "url": "/static/js/2.73fd23be.chunk.js"
+  },
+  {
+    "revision": "714c147d840f36980f507a00da3599d3",
+    "url": "/static/js/2.73fd23be.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d3c353a5179f743018c7",
+    "url": "/static/js/main.ef6e3ae2.chunk.js"
+  },
+  {
+    "revision": "7269b0c4207b44d8a9e4f0b85c8a81d2",
+    "url": "/static/js/main.ef6e3ae2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "824448d6be5d10f46b59",
+    "url": "/static/js/runtime-main.ca5d16f1.js"
+  },
+  {
+    "revision": "600695d09aba3a355a06f9bae586174c",
+    "url": "/static/media/MissingPhotoAvatar.600695d0.jpg"
+  },
+  {
+    "revision": "689e7d30d0112a3b28f0da450ff1a859",
+    "url": "/static/media/parliamentvertical.689e7d30.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/PocketParliament/precache-manifest.480706298555179c38c48d3dbc2e862f.js"
+  "/precache-manifest.1eb0198a52dfd3850f806e3ae155dec6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/PocketParliament/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
